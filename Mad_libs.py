75b85e8b7a98c5a60869ec4f9a4929a534e59413
Prajwol <prajwolshrestha08@gmail.com>
2020-08-22 09:14:34 +0545

    Mad_libs

---
+++ Mad_libs.py
@@ -0,0 +1,18 @@
+first_word = input("Enter first word: ")
+second_word = input("Enter second word: ")
+third_word = input("Enter third word: ")
+fourth_word = input("Enter fourth word: ")
+fifth_word = input("Enter fifth word: ")
+
+print("\n\n")
+
+
+print("Oh, won't you tell me? Please just " + first_word)  
+print("Explain how this " +second_word + " work")
+print("Well now who could it be, that lives inside of me?")
+print("I'm broken, lying helpless, shattered")
+print("Surrounded by the " + third_word)
+print("And yet, your smiling " +fourth_word)
+print("Completely blind to life")
+print("My ruptured lungs; they were left this way")
+print("For once, I'm out of " + fifth_word)
